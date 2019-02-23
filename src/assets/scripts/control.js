d4b4e790ad74ab88e128fb611675b91f7bacc153
Ari <aristath@gmail.com>
2019-02-24 01:07:21 +0200

    add kirki.control script

--- src/assets/scripts/control.js
+++ src/assets/scripts/control.js
@@ -40,3 +40,66 @@ kirki.input.textarea = {
         } );
     }
 };
+
+kirki.control = kirki.control || {};
+kirki.control['kirki-generic'] = {
+
+    /**
+     * Init the control.
+     *
+     * @since 3.0.17
+     * @param {Object} control - The customizer control object.
+     * @param {Object} control.params - Control parameters.
+     * @param {Object} control.params.choices - Define the specifics for this input.
+     * @param {string} control.params.choices.element - The HTML element we want to use ('input', 'div', 'span' etc).
+     * @returns {null}
+     */
+    init: function( control ) {
+        var self = this;
+
+        // Render the template.
+        self.template( control );
+
+        // Init the control.
+        if ( ! _.isUndefined( control.params ) && ! _.isUndefined( control.params.choices ) && ! _.isUndefined( control.params.choices.element ) && 'textarea' === control.params.choices.element ) {
+            kirki.input.textarea.init( control );
+            return;
+        }
+        kirki.input.genericInput.init( control );
+    },
+
+    /**
+     * Render the template.
+     *
+     * @since 3.0.17
+     * @param {Object}  control - The customizer control object.
+     * @param {Object}  control.params - The control parameters.
+     * @param {string}  control.params.label - The control label.
+     * @param {string}  control.params.description - The control description.
+     * @param {string}  control.params.inputAttrs - extra input arguments.
+     * @param {string}  control.params.default - The default value.
+     * @param {Object}  control.params.choices - Any extra choices we may need.
+     * @param {boolean} control.params.choices.alpha - should we add an alpha channel?
+     * @param {string}  control.id - The setting.
+     * @returns {null}
+     */
+    template: function( control ) {
+        var args = {
+                label: control.params.label,
+                description: control.params.description,
+                'data-id': control.id,
+                inputAttrs: control.params.inputAttrs,
+                choices: control.params.choices,
+                value: kirki.setting.get( control.id )
+            },
+            template;
+
+        if ( ! _.isUndefined( control.params ) && ! _.isUndefined( control.params.choices ) && ! _.isUndefined( control.params.choices.element ) && 'textarea' === control.params.choices.element ) {
+            template = wp.template( 'kirki-input-textarea' );
+            control.container.html( template( args ) );
+            return;
+        }
+        template = wp.template( 'kirki-input-generic' );
+        control.container.html( template( args ) );
+    }
+};
