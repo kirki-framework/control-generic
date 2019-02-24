f4407d80532e73589ec890f5a0ab345885335a3c
Ari <aristath@gmail.com>
2019-02-24 12:42:59 +0200

    add view

---
+++ src/functions.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Add extra functions and hooks.
+ *
+ * @package    Kirki
+ * @copyright  Copyright (c) 2019, Ari Stathopoulos (@aristath)
+ * @license    https://opensource.org/licenses/MIT
+ * @since      4.0
+ */
+
+add_action(
+    'customize_register',
+    function() {
+        class_alias( 'Kirki\Control\Generic', 'Kirki_Control_Generic' );
+    }
+);
+
+add_action(
+    'customize_controls_print_footer_scripts',
+    function() {
+        $path = apply_filters( 'kirki_control_view_generic', __DIR__ . '/view.php' );
+        echo '<script type="text/html" id="tmpl-kirki-input-generic">';
+        include $path;
+        echo '</script>';
+    }
+);
