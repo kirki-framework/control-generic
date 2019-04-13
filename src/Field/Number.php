fc0da746fd4f625552a66a8565368c4f5eb8d54c
Ari <aristath@gmail.com>
2019-04-14 01:18:04 +0300

    Added number

---
+++ src/Field/Number.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Override field methods
+ *
+ * @package   kirki-framework/control-generic
+ * @copyright Copyright (c) 2019, Ari Stathopoulos (@aristath)
+ * @license   https://opensource.org/licenses/MIT
+ * @since     1.0
+ */
+
+namespace Kirki\Field;
+
+/**
+ * Field overrides.
+ *
+ * @since 1.0
+ */
+class Number extends Generic {
+
+	/**
+	 * Sets the $choices
+	 *
+	 * @access protected
+	 * @since 1.0
+	 * @return void
+	 */
+	protected function set_choices() {
+		if ( ! is_array( $this->choices ) ) {
+			$this->choices = [];
+		}
+		$this->choices['element'] = 'input';
+        $this->choices['type']    = 'number';
+        $this->choices = wp_parse_args(
+			$this->choices,
+			[
+				'min'  => -999999999,
+				'max'  => 999999999,
+				'step' => 1,
+			]
+		);
+		// Make sure min, max & step are all numeric.
+		$this->choices['min']  = filter_var( $this->choices['min'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+		$this->choices['max']  = filter_var( $this->choices['max'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+		$this->choices['step'] = filter_var( $this->choices['step'], FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+	}
+
+	/**
+	 * Sets the $sanitize_callback
+	 *
+	 * @access protected
+	 * @since 1.0
+	 * @return void
+	 */
+	protected function set_sanitize_callback() {
+		if ( ! empty( $this->sanitize_callback ) ) {
+			$this->sanitize_callback = [ 'Kirki\Field\Number', 'sanitize' ];
+		}
+    }
+    
+    /**
+	 * Sanitizes numeric values.
+	 *
+	 * @access public
+	 * @since 1.0
+	 * @param integer|string $value The checkbox value.
+	 * @return bool
+	 */
+	public function sanitize( $value = 0 ) {
+        return filter_var( $value, FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION );
+    }
+}
